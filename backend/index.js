a310750fdf1c9c7cec96078cd360913ae30ca5b1
Shivam Yadav <shivamptu4566@gmail.com>
2026-06-21 23:33:19 +0530

    Update CORS origin for production environment

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ let app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
- origin:["http://localhost:5173" , "http://localhost:5174"],
+ origin:["https://ai-ecommerce-1-frontend.onrender.com" , "https://ai-ecommerce-1-admin.onrender.com"],
  credentials:true
 }))
 
